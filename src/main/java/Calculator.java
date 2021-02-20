bb1a1bb0a391a5f9f3f5a08650d5121051ab3982
Josh <josh.reese.is@gmail.com>
2021-02-19 21:35:18 -0700

    Implement FibonacciNumberFinder

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -38,8 +38,10 @@ class Calculator {
     .
     etc
      */
-    int fibonacciNumberFinder(int n){
-        return 0;
+    int fibonacciNumberFinder(int n) {
+        if (n <= 1)
+            return n;
+        return fibonacciNumberFinder(n-1) + fibonacciNumberFinder(n-2);
     }
 
 
